8b7ece0f5b0cc3b871506cd238aed9bb6a467f8f
user <sudhandhakshneswaran@gmail.com>
2020-08-27 01:03:27 +0530

    second commit, Back end completed

--- springsql/src/main/java/com/example/springsql/controller/CurdRestController.java
+++ springsql/src/main/java/com/example/springsql/controller/CurdRestController.java
@@ -1,11 +1,53 @@
 package com.example.springsql.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springsql.modal.Product;
+import com.example.springsql.services.CrudServices;
 
 @RestController
 @ComponentScan
 public class CurdRestController {
 
+	@Autowired
+	private CrudServices service;
 	
+	@GetMapping("/getproductlist")
+	public List<Product> fetchProductList()
+	{
+		List<Product> productList = new ArrayList<Product>();
+		//Logic to fetch list from database
+		productList = service.fetchProductList();
+		return productList;
+	}
+	
+	@PostMapping("/addproduct")
+	public Product saveProduct(@RequestBody Product product)
+	{
+		return service.saveProductToDB(product);
+	}
+	
+	@GetMapping("/getproductbyid/{id}")
+	public Product fetchProductById(@PathVariable int id)
+	{
+		return service.fetchProductById(id).get();
+	}
+	
+	@DeleteMapping("/removeproductbyid/{id}")
+	public String deleteProduct(@PathVariable int id)
+	{
+		return service.deleteProductById(id);
+	}
 }
--- springsql/src/main/java/com/example/springsql/services/CrudServices.java
+++ springsql/src/main/java/com/example/springsql/services/CrudServices.java
@@ -1,10 +1,51 @@
 package com.example.springsql.services;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
+
+import com.example.springsql.Repo.CrudRepo;
+import com.example.springsql.modal.Product;
+
 
 @ComponentScan
 @Service
 public class CrudServices {
+	@Autowired
+	private CrudRepo repo;
+	
+	public List<Product> fetchProductList()
+	{
+		return repo.findAll();
+	}
+	
+	public Product  saveProductToDB(Product product)
+	{
+		return repo.save(product);
+	}
+	
+	public Optional<Product> fetchProductById(int id)
+	{
+		return repo.findById(id);
+	}
+	
+	public String deleteProductById(int id)
+	{
+		String result;
+		try 
+		{
+			 repo.deleteById(id);
+			 result = "Product Removed Successfully";
+		}
+		catch(Exception e)
+		{
+			result = "Exception in Removing Product";
+		}
+		return result;
+	}
 
 }
